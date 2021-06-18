bfdca0f31f661bbba3a2be7376cbe37f4bb255e2
mathieuelisa <mathieu.elisa@gmail.com>
2021-06-18 19:30:37 +0200

    Update CSS on Home page

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-//jshint esversion:6
-
 const express = require("express");
 const ejs = require("ejs");
-const _ = require("lodash")
-const mongoose = require("mongoose")
-const router = require("./router")
+const _ = require("lodash");
+const mongoose = require("mongoose");
+const router = require("./router");
 
 const app = express();
 
 // Moteur de template
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Middlewares
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(router)
+app.use(router);
 
 // Mongoose connection
-mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
-
-// Server
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+// Server
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
